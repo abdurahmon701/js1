1dc66cc18172964b9d7bc708f0d9f59c3b696c5b
abdurahmon70101~ <abdurahmonabdumavlonov750@gmail.com>
2024-08-15 01:17:05 +0500

    sjcsjr csjc

---
+++ script.js
@@ -0,0 +1,22 @@
+const ism = prompt('Ismingizni kiriting')
+
+const familiya = prompt('Familiyangizni kiriting')
+
+const yosh = prompt('Yoshingizni kiriting')
+
+const fan = prompt('Yoqtirgan faningizni kiriting')
+
+const xudud = prompt('Yashaydigan xududingizni kiriting')
+
+alert('Malumotingizni kiritganingiz uchun tashakkur')
+
+console.log('Ism:', ism)
+
+console.log('Familiya:' ,familiya)
+
+console.log('Yosh:', yosh)
+
+console.log('Yoqtirgan fan:', fan)
+
+console.log('Yashash manzil:', xudud)
+
